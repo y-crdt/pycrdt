3431b2d7b2707c0b50b77fdf1a4bcca326d2dc18
David Brochart <david.brochart@gmail.com>
2025-04-30 08:59:16 +0200

    Move Channel to _provider.py

--- python/pycrdt/_provider.py
+++ python/pycrdt/_provider.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from contextlib import AsyncExitStack, asynccontextmanager
 from logging import Logger, getLogger
-from typing import AsyncIterator
+from typing import AsyncIterator, Protocol
 
 from anyio import (
     TASK_STATUS_IGNORED,
@@ -13,7 +13,6 @@ from anyio import (
 )
 from anyio.abc import TaskGroup, TaskStatus
 
-from ._channel import Channel
 from ._doc import Doc
 from ._pycrdt import TransactionEvent
 from ._sync import (
@@ -23,6 +22,54 @@ from ._sync import (
     create_update_message,
     handle_sync_message,
 )
+
+
+class Channel(Protocol):
+    """A transport-agnostic stream used to synchronize a document through a provider.
+    An example of a channel is a WebSocket.
+
+    Messages can be received through the channel using an async iterator,
+    until the connection is closed:
+    ```py
+    async for message in channel:
+        ...
+    ```
+    Or directly by calling `recv()`:
+    ```py
+    message = await channel.recv()
+    ```
+    Sending messages is done with `send()`:
+    ```py
+    await channel.send(message)
+    ```
+    """
+
+    @property
+    def path(self) -> str:
+        """The channel path."""
+        ...  # pragma: nocover
+
+    def __aiter__(self) -> "Channel":
+        return self
+
+    async def __anext__(self) -> bytes:
+        return await self.recv()
+
+    async def send(self, message: bytes) -> None:
+        """Send a message.
+
+        Args:
+            message: The message to send.
+        """
+        ...  # pragma: nocover
+
+    async def recv(self) -> bytes:
+        """Receive a message.
+
+        Returns:
+            The received message.
+        """
+        ...  # pragma: nocover
 
 
 class Provider:
