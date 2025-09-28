eeadd033760a0f50cb4d41a424f1c16be7d4897e
David Brochart <david.brochart@gmail.com>
2025-09-28 20:33:36 +0200

    Drop dependency on importlib_metadata (#305)

--- python/pycrdt/_version.py
+++ python/pycrdt/_version.py
@@ -1,10 +1,4 @@
-try:
-    from importlib.metadata import PackageNotFoundError, version
-except ImportError:  # pragma: no cover
-    from importlib_metadata import (  # type: ignore[assignment, import-not-found, no-redef]
-        PackageNotFoundError,
-        version,
-    )
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("pycrdt")
